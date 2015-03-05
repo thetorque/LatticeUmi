b851b8c9209000f43681203982748d75fc67fb4c
thetorque <thanedp@gmail.com>
2015-03-05 14:36:55 +0900

    Test programming of pulser and bus connection between pulser and dds

--- dev/pulser/pulser_test.py
+++ dev/pulser/pulser_test.py
@@ -1,9 +1,13 @@
 import ok
+import time
 xem = ok.FrontPanel()
 xem.OpenBySerial('')
 #xem.ConfigureFPGA('C:\Users\Thaned\Desktop\Hardware\RIKEN_Hardware\Pulser_Altera\output_files\Pulser.rbf')
 
 # set wire in (FPGA reads data). First is the address, second is the value, last is the masking (1 for the bit you want to change)
-xem.SetWireInValue(0x00,0b10101000,0xF8)
+##xem.SetWireInValue(0x00,0b10101000,0xF8)
 # after this you have to update the wire in
+##xem.UpdateWireIns()
+
+xem.SetWireInValue(0x00,0x00000000,0xFFFFFFFF)
 xem.UpdateWireIns()
