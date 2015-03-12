e7c4424e2571e21c5c052e1738f2c1710f268296
thetorque <thanedp@gmail.com>
2015-03-12 20:28:49 +0900

    test fine tuning of frequency and 2 channels dds

--- dev/pulser/old_pulser_test_dds_fine_tuning.py
+++ dev/pulser/old_pulser_test_dds_fine_tuning.py
@@ -7,31 +7,57 @@ def padTo16(data):
     zero_padding = bytearray(size_needed)
     return data+zero_padding
 
+
+def freqToByteArray(freq):
+    f_ref = 2000000000.0
+    #input freq in Hz
+    f_int = int(freq*(2**32)*(2**32)/f_ref)
+    print "freq is =",f_int
+    b = bytearray(8)
+    for i in range(8):
+        b[i]=(f_int//(2**(i*8)))%256
+        print i, "=", (f_int//(2**(i*8)))%256
+    return b
+
+def ampToByteArray(amp):
+    ## 0 to 65535 (actually only 14 bits)
+    b = bytearray(2)
+    b[0] = amp%256
+    b[1] = (amp//256)%256
+    data = bytearray.fromhex(u'0000') + b + bytearray.fromhex(u'0000 0000') ### zeros stuff are unused for now
+    return data 
+
+def DDSdata(amp,freq):
+    c = ampToByteArray(amp) + freqToByteArray(freq)
+    return c
+
 xem = ok.FrontPanel()
 xem.OpenBySerial('')
 #xem.ConfigureFPGA('C:\Users\Thaned\Dropbox\Hardware\RIKEN_Hardware\Pulser_Altera\output_files\Pulser.rbf')
 xem.ConfigureFPGA('C:\Users\Thaned\Dropbox\Hardware\RIKEN_Hardware\Pulser_w_line_triggering_2015_03_10\photon\photon.bit')
 delay = 0.1
-
+ 
 padding = bytearray.fromhex(u'0000') 
-setting_0 = bytearray.fromhex(u'0000 00b0 0000 0009') ## []frequency[7 to 0][15 to 8][23 to 16][31 to 24]
-setting_1 = bytearray.fromhex(u'0000 00b0 0000 0008')
-setting_2 = bytearray.fromhex(u'0000 00b0 0000 0007')
-setting_3 = bytearray.fromhex(u'0000 00c0 0000 0006')
-data = padding + setting_0+setting_1+setting_2+setting_3
-data = padTo16(data)
+# # #amp_0 = bytearray.fromhex(u'0000 00b0 0000 0000') ## []frequency[7 to 0][15 to 8][23 to 16][31 to 24] ## this goes to lower chuck
+# # amp_0 = ampToByteArray(50000)
+# # #setting_1 = bytearray.fromhex(u'0000 0000 0000 0008') ## lower:frequency[7 to 0][15 to 8][23 to 16][31 to 24] higher:[7 to 0][15 to 8][23 to 16][31 to 24]
+# # freq_0 = freqToByteArray(80000000.1)
+# # amp_1 = bytearray.fromhex(u'0000 00b0 0000 0000')
+# # freq_1 = freqToByteArray(80000000.2)
+# data = padding + DDSdata(40000,80000000) + DDSdata(40000,80000001)
+# data = padTo16(data)
+#  
+# xem.ActivateTriggerIn(0x40,4)
+#  
+# xem.SetWireInValue(0x04,0x00,0xFF)
+# xem.UpdateWireIns()
+#  
+# xem.WriteToBlockPipeIn(0x81,16,data)
 
-
-xem.ActivateTriggerIn(0x40,4)
-
-xem.SetWireInValue(0x04,0x00,0xFF)
-xem.UpdateWireIns()
-
-xem.WriteToBlockPipeIn(0x81,16,data)
-
-time.sleep(1)
-xem.ActivateTriggerIn(0x40,5)
-time.sleep(1)
-xem.ActivateTriggerIn(0x40,5)
-time.sleep(1)
-xem.ActivateTriggerIn(0x40,5)
+for i in range(1000):
+    time.sleep(2)
+    print i
+    xem.ActivateTriggerIn(0x40,4)
+    data = padding + DDSdata(40000,80000000+i/10000.0)
+    data = padTo16(data)
+    xem.WriteToBlockPipeIn(0x81,16,data)
